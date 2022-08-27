8820f2e4b0ffc40871f8a93638a79528d2cf2bff
paulweezydesign <weezy@paulweezydesign.com>
2022-08-26 21:14:38 -0700

    added tailwind, alpinejs and navbar, and layed out the initial stucture.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,js,html,css}', './index.html','./tailwind.css'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
